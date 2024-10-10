86a8f2a89a097374a3f9a9dca33f8355bb6ca950
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-10-10 11:23:47 +0530

    completed wallet convertor functions

--- enrollment-dapp/src/lib.rs
+++ enrollment-dapp/src/lib.rs
@@ -1,9 +1,11 @@
 #[cfg(test)]
 mod tests {
+    use bs58;
     use solana_sdk::{
         pubkey::Pubkey,
         signature::{Keypair, Signer},
     };
+    use std::io::{self, stdin, BufRead};
 
     #[test]
     fn keygen() {
@@ -23,4 +25,34 @@ mod tests {
 
     #[test]
     fn transfer_sol() {}
+
+    #[test]
+    fn base58_to_wallet() {
+        println!("Input your private key as base58: ");
+        let stdin = io::stdin();
+        let base58 = stdin.lock().lines().next().unwrap().unwrap();
+        println!("Your wallet file is: ");
+        let wallet = bs58::decode(base58).into_vec().unwrap();
+        println!("{:?}", wallet);
+    }
+
+    #[test]
+    fn wallet_to_base58() {
+        println!("Input your private key as a wallet file byte array: ");
+        let stdin = io::stdin();
+        let wallet = stdin
+            .lock()
+            .lines()
+            .next()
+            .unwrap()
+            .unwrap()
+            .trim_start_matches('[')
+            .trim_end_matches(']')
+            .split(',')
+            .map(|s| s.trim().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+        println!("Your private key is: ");
+        let base58 = bs58::encode(wallet).into_string();
+        println!("{:?}", base58);
+    }
 }
