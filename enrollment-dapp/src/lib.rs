858de2a9e7c1c00a50d628c98dfff32da59341e4
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-10-10 10:59:57 +0530

    completed rust keygen assignment

--- enrollment-dapp/src/lib.rs
+++ enrollment-dapp/src/lib.rs
@@ -1,12 +1,22 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use solana_sdk;
+    use solana_sdk::{
+        pubkey::Pubkey,
+        signature::{Keypair, Signer},
+    };
+
     #[test]
-    fn keygen() {}
+    fn keygen() {
+        let kp = Keypair::new();
+        println!(
+            "You've generated a new Solana Wallet: {}",
+            kp.pubkey().to_string()
+        );
+        println!("");
+        println!("To save your wallet, copy and paste the following into a JSON file: ");
+        println!("{:?}", kp.to_bytes());
+        //Dev wallet - J5KD3VXQouUnuQwDJ7K7xzcyeYpWXTDqbdE69CyiHCu
+    }
 
     #[test]
     fn airdrop() {}
