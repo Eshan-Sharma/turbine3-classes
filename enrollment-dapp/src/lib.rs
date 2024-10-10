a1070ef02176154de67a1d149d0a378c1add8f44
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-10-10 13:11:52 +0530

    completed enroll assignment

--- enrollment-dapp/src/lib.rs
+++ enrollment-dapp/src/lib.rs
@@ -1,3 +1,5 @@
+mod programs;
+use crate::programs::Turbin3_prereq::{CompleteArgs, Turbin3PrereqProgram, UpdateArgs};
 #[cfg(test)]
 mod tests {
     use bs58;
@@ -5,13 +7,47 @@ mod tests {
     use solana_program::{pubkey::Pubkey, system_instruction::transfer};
     use solana_sdk::{
         message::Message,
-        signature::{read_keypair_file, Keypair, Signer},
-        signer::keypair,
+        signature::{self, read_keypair_file, Keypair, Signer},
+        signer::{self, keypair},
+        system_program,
         transaction::Transaction,
     };
     use std::io::{self, stdin, BufRead};
     use std::str::FromStr;
+
+    use crate::programs::Turbin3_prereq::{CompleteArgs, Turbin3PrereqProgram};
     const RPC_URL: &str = "https://api.devnet.solana.com";
+
+    #[test]
+    fn enroll() {
+        let rpc_client = RpcClient::new(RPC_URL);
+        let signer = read_keypair_file("Turbine3-wallet.json").expect("Couldn't find wallet file");
+        let prereq = Turbin3PrereqProgram::derive_program_address(&[
+            b"prereq",
+            signer.pubkey().to_bytes().as_ref(),
+        ]);
+        let args = CompleteArgs {
+            github: b"eshan-Sharma".to_vec(),
+        };
+        let blockhash = rpc_client
+            .get_latest_blockhash()
+            .expect("Failed to get recent blockhash");
+        let transaction = Turbin3PrereqProgram::complete(
+            &[&signer.pubkey(), &prereq, &system_program::id()],
+            &args,
+            Some(&signer.pubkey()),
+            &[&signer],
+            blockhash,
+        );
+        let signature = rpc_client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send transaction");
+        println!(
+            "Success! Checkout your TX here: https://explorer.solana.com/tx/{}?cluster=devnet",
+            signature
+        );
+        //Transaction - https://explorer.solana.com/tx/2JmJXvsquxy5aKTDpwYYaYDETBDzgCPkxqQiqGQCSmcjMjUbhdReUfuxZyyyuFzcWpiPdHNp5viRCQGoHTjYHVbr?cluster=devnet
+    }
 
     #[test]
     fn keygen() {
